a08ad96d1720c4d5483165868335306d30cb5212
Olusolaadi <olusolafatunde@gmail.com>
2024-10-21 17:24:24 -0500

    backend

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.js";
 
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 7000;
 
-// ===== Connect to DB ===== //
+// ===== Connect to MongoDB ===== //
 try {
   await mongoose.connect(process.env.MONGODB_URI);
   console.log(`Connected to mongodb`);
@@ -19,9 +20,10 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 
 app.get("/", (req, res) => {
+  res.status(200);
   res.send("<h1>Welcome to my API</h1>");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
